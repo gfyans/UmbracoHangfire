517f6756b5175c52777636be5b6b89304c643bca
Greg Fyans <greg@screenmedia.co.uk>
2016-02-22 10:03:08 +0000

    Initial commit

---
+++ Umbraco741Hangfire/OwinStartup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Owin;
+using Umbraco.Web;
+
+namespace Umbraco741Hangfire
+{
+    public class OwinStartup : UmbracoDefaultOwinStartup
+    {
+        public override void Configuration(IAppBuilder app)
+        {
+            base.Configuration(app);
+
+            Console.WriteLine("Setting up Hangfire...");
+        }
+    }
+}
